0a55c9a23e3d983f11ff105dd75952a6da649ef2
Roger Huang <roger@beautimode.com>
2020-08-25 17:32:17 +0800

    * Support settings rtlVerticalLayout and useFontDFKaiSB.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ccd6989cd9f4ea1fc302efee468d6bf1.js"
+  "/precache-manifest.239ee370066a67d3dfe97bd89fee8231.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

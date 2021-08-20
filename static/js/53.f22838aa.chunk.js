51b1e1e3b0b62e79112730fdabafbc1b9837c3ef
Meng-Yuan Huang <myh@live.com>
2021-08-20 17:55:43 +0800

    4.23.8

---
+++ static/js/53.f22838aa.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcbetar2=this.webpackJsonpcbetar2||[]).push([[53],{498:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var o=e(28),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var i=c.closest("ion-content");i&&i.componentOnReady().then((function(){Object(o.f)((function(){return i.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.f22838aa.chunk.js.map

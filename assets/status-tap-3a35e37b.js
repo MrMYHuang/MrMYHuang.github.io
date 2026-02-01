c1e34ae83fcab6d29341f9005a0f4f02b636aa3d
Meng-Yuan Huang <myh@live.com>
2026-02-01 15:09:45 +0800

    10.11.1

--- assets/status-tap-3a35e37b.js
+++ assets/status-tap-3a35e37b.js
@@ -1,4 +1,4 @@
-import{r as a,f as i,b as c,w as d,s as l}from"./index-db5311ad.js";/*!
+import{r as a,f as i,b as c,w as d,s as l}from"./index-542778eb.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
-//# sourceMappingURL=status-tap-b9e689ff.js.map
+//# sourceMappingURL=status-tap-3a35e37b.js.map
